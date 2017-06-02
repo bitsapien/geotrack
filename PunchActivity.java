c5e4ba043f1903e2343307791c39a7a422110c39
bitsapien <bitsapien@gmail.com>
2017-06-02 17:17:04 +0530

    camera,gps done

--- PunchActivity.java
+++ PunchActivity.java
@@ -1,14 +1,18 @@
 package com.example.crahul.geotrack;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -18,6 +22,10 @@ public class PunchActivity extends AppCompatActivity {
     private TrackGPS gps;
     private double latitude;
     private double longitude;
+    private static final int CAMERA_REQUEST = 1888;
+    long newRowId;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,11 +77,11 @@ public class PunchActivity extends AppCompatActivity {
             String geo_long = cursor.getString(
                     cursor.getColumnIndexOrThrow(GeoTrackDBContract.Attendance.COLUMN_GEO_LONG)
             );
-            String photo = cursor.getString(
-                    cursor.getColumnIndexOrThrow(GeoTrackDBContract.Attendance.COLUMN_PHOTO)
-            );
+//            String photo = cursor.getString(
+//                    cursor.getColumnIndexOrThrow(GeoTrackDBContract.Attendance.COLUMN_PHOTO)
+//            );
 
-            System.out.println("UserID: "+user_id+" CreatedAt : "+created_at+" LAT: "+geo_lat+" LONG: "+geo_long+" PHOTO: "+photo);
+            System.out.println("UserID: "+user_id+" CreatedAt : "+created_at+" LAT: "+geo_lat+" LONG: "+geo_long);
         }
         cursor.close();
 
@@ -81,12 +89,14 @@ public class PunchActivity extends AppCompatActivity {
     }
 
     private void saveToDB() {
-        SQLiteDatabase database = new GeoTrackDBSQLiteHelper(this).getWritableDatabase();
 
+        SQLiteDatabase database = new GeoTrackDBSQLiteHelper(this).getWritableDatabase();
         ContentValues values = new ContentValues();
 
+        // Datetime
         String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
 
+        // GPS
         gps = new TrackGPS(PunchActivity.this);
         if(gps.canGetLocation()){
 
@@ -102,6 +112,9 @@ public class PunchActivity extends AppCompatActivity {
             gps.showSettingsAlert();
         }
 
+        // Camera
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
 
         values.put(GeoTrackDBContract.Attendance.COLUMN_USER_ID, "1");
         values.put(GeoTrackDBContract.Attendance.COLUMN_GEO_LAT, Double.toString(latitude));
@@ -109,8 +122,24 @@ public class PunchActivity extends AppCompatActivity {
         values.put(GeoTrackDBContract.Attendance.COLUMN_CREATED_AT, currentDateTimeString);
 
 
-        long newRowId = database.insert(GeoTrackDBContract.Attendance.TABLE_NAME, null, values);
+
+        newRowId = database.insert(GeoTrackDBContract.Attendance.TABLE_NAME, null, values);
 
         Toast.makeText(this, "The new Row Id is " + newRowId, Toast.LENGTH_LONG).show();
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        SQLiteDatabase database = new GeoTrackDBSQLiteHelper(this).getWritableDatabase();
+        if (requestCode == CAMERA_REQUEST) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] imageByteArray = stream.toByteArray();
+            ContentValues values = new ContentValues();
+            values.put(GeoTrackDBContract.Attendance.COLUMN_PHOTO, imageByteArray);
+            String[] selectionArgs = {Long.toString(newRowId)};
+
+            database.update(GeoTrackDBContract.Attendance.TABLE_NAME,values,GeoTrackDBContract.Attendance._ID+"=?",selectionArgs);
+
+        }
     }
 }
